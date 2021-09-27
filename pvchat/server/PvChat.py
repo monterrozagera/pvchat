f7fa0774a3178652dc25900e607e754dfd68aad9
monterrozagera <monterrozagerardo@protonmail.com>
2021-09-26 21:16:38 -0700

    add logo in server

--- pvchat/server/PvChat.py
+++ pvchat/server/PvChat.py
@@ -6,6 +6,17 @@ import argparse
 import time
 import sys
 
+logo = """ ██▓███   ██▒   █▓ ▄████▄   ██░ ██  ▄▄▄     ▄▄▄█████▓
+▓██░  ██▒▓██░   █▒▒██▀ ▀█  ▓██░ ██▒▒████▄   ▓  ██▒ ▓▒
+▓██░ ██▓▒ ▓██  █▒░▒▓█    ▄ ▒██▀▀██░▒██  ▀█▄ ▒ ▓██░ ▒░
+▒██▄█▓▒ ▒  ▒██ █░░▒▓▓▄ ▄██▒░▓█ ░██ ░██▄▄▄▄██░ ▓██▓ ░ 
+▒██▒ ░  ░   ▒▀█░  ▒ ▓███▀ ░░▓█▒░██▓ ▓█   ▓██▒ ▒██▒ ░ 
+▒▓▒░ ░  ░   ░ ▐░  ░ ░▒ ▒  ░ ▒ ░░▒░▒ ▒▒   ▓▒█░ ▒ ░░   
+░▒ ░        ░ ░░    ░  ▒    ▒ ░▒░ ░  ▒   ▒▒ ░   ░    
+░░            ░░  ░         ░  ░░ ░  ░   ▒    ░      
+               ░  ░ ░       ░  ░  ░      ░  ░        
+              ░   ░                                 """
+
 # IP and PORT
 HOST, PORT = 'localhost', 9999
 
@@ -15,25 +26,28 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--key', type=str, help='Private RSA key to start server.')
     args = parser.parse_args()
+    print()
+    print(logo)
+    print()
 
-    print("Initializing server on {}:{}".format(HOST, PORT))
+    print("[!] Initializing server on {}:{}".format(HOST, PORT))
 
     # start auth service
     auth = server_authentication.Authentication()
 
     if args.key:
-        print("Loading key..")
+        print("[*] Loading key..")
         private_key = auth.loadKey(args.key)
         time.sleep(2)
-        print("OK!")
+        print("[!] OK")
     else:
-        print("No key provided.")
+        print("[!] No key provided.")
         print()
-        generate_key = input("Generate key? y/n\n")
+        generate_key = input("[*] Generate key? y/n\n")
         if generate_key == 'y':
             new_key = auth.generateKey()
             print()
-            print("New key generated! Now load it with the --key arg.")
+            print("[!] New key generated! Now load it with the --key arg.")
             sys.exit(0)
         else:
             sys.exit(0)
